4c8135c64dbc33aa6d1a730cbcd4d1a9ca164007
yurjune <yurjune@naver.com>
2026-02-25 09:44:57 +0900

    refactor: contents table

--- components/ContentsTableView/ContentsTable.jsx
+++ components/ContentsTableView/ContentsTable.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Link from "next/link";
-import { Box } from "@chakra-ui/react";
+import { useMediaQuery, useTheme } from "@chakra-ui/react";
 import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 import { categoryToContents } from "../../utils/categoryConverter";
 import { getLimitedArticles } from "../../hooks/useArticle";
@@ -13,90 +13,48 @@ const noWrap = {
   textOverflow: "ellipsis",
 };
 
-const DesktopTable = ({ articles, tableStyle }) => {
-  const title = {
-    maxWidth: "0", // 레이아웃 탈영방지
-    width: "60%", // 제목영역 크기조절
-  };
+const ContentsTable = ({ articles: _articles, currentPage = 1 }) => {
+  const articles = getLimitedArticles(_articles, currentPage);
+  const theme = useTheme();
+  const [isDesktop] = useMediaQuery(`(min-width: ${theme.breakpoints.md})`);
+
   return (
-    <Box display={{ base: "none", md: "block" }}>
-      <Table {...tableStyle}>
-        <Thead>
-          <Tr>
-            <Th {...noWrap}>카테고리</Th>
-            <Th>작성자</Th>
-            <Th>제목</Th>
+    <Table colorScheme="blackAlpha" variant="striped">
+      <Thead>
+        <Tr>
+          <Th {...noWrap}>카테고리</Th>
+          {isDesktop && <Th>작성자</Th>}
+          <Th>제목</Th>
+          {isDesktop && (
             <Th {...noWrap} isNumeric>
               일시
             </Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {articles.length >= 1 &&
-            articles.map((article) => (
-              <Tr key={article.sys.id}>
-                <Td {...noWrap}>{article.fields.category}</Td>
-                <Td {...noWrap}>디딤돌</Td>
-                <Td {...title}>
-                  <Link
-                    href={`${categoryToContents(article.fields.category)}/${article.sys.id}`}
-                  >
-                    {article.fields.title}
-                  </Link>
-                </Td>
+          )}
+        </Tr>
+      </Thead>
+
+      <Tbody>
+        {articles.length >= 1 &&
+          articles.map((article) => (
+            <Tr key={article.sys.id}>
+              <Td {...noWrap}>{article.fields.category}</Td>
+              {isDesktop && <Td {...noWrap}>관리자</Td>}
+              <Td maxWidth={0} w="60%">
+                <Link
+                  href={`${categoryToContents(article.fields.category)}/${article.sys.id}`}
+                >
+                  {article.fields.title}
+                </Link>
+              </Td>
+              {isDesktop && (
                 <Td {...noWrap} isNumeric>
-                  {article.sys.createdAt.slice(5, 10)}
+                  {article.sys.createdAt.slice(0, 10)}
                 </Td>
-              </Tr>
-            ))}
-        </Tbody>
-      </Table>
-    </Box>
-  );
-};
-
-const MobileTable = ({ articles, tableStyle }) => {
-  const title = {
-    maxWidth: "0",
-    width: "70%",
-  };
-  return (
-    <Box display={{ base: "block", md: "none" }}>
-      <Table {...tableStyle}>
-        <Thead>
-          <Tr>
-            <Th {...noWrap}>카테고리</Th>
-            <Th>제목</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {articles.length >= 1 &&
-            articles.map((article) => (
-              <Tr key={article.sys.id}>
-                <Td {...noWrap}>{article.fields.category}</Td>
-                <Td {...title}>
-                  <Link
-                    href={`${categoryToContents(article.fields.category)}/${article.sys.id}`}
-                  >
-                    {article.fields.title}
-                  </Link>
-                </Td>
-              </Tr>
-            ))}
-        </Tbody>
-      </Table>
-    </Box>
-  );
-};
-
-const ContentsTable = ({ articles, tableStyle, currentPage = 1 }) => {
-  const limitedArticles = getLimitedArticles(articles, currentPage);
-
-  return (
-    <>
-      <DesktopTable articles={limitedArticles} tableStyle={tableStyle} />
-      <MobileTable articles={limitedArticles} tableStyle={tableStyle} />
-    </>
+              )}
+            </Tr>
+          ))}
+      </Tbody>
+    </Table>
   );
 };
 
