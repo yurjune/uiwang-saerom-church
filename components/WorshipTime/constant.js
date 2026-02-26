3843fca20b93da6ac464422a08902ee63f4e0b33
yurjune <yurjune@naver.com>
2026-02-27 08:46:38 +0900

    feat: change worship time

--- components/WorshipTime/constant.js
+++ components/WorshipTime/constant.js
@@ -1,6 +1,6 @@
 export const worshipSchedule = [
   { title: "주일예배", day: "매주 일요일 오전", time: "11:00" },
   { title: "오후모임", day: "매주 일요일 오후", time: "14:00" },
-  { title: "주일학교", day: "매주 일요일 오전", time: "10:00" },
+  { title: "주일학교", day: "매주 일요일 오전", time: "09:30" },
   { title: "수요모임", day: "매주 수요일 오후", time: "20:00" },
 ];
