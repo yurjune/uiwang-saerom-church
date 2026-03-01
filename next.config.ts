dda1fb9fb0d03afebd7d3828dabf89596b12c05b
yurjune <yurjune@naver.com>
2026-03-02 03:03:17 +0900

    refactor: migrate next config

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["images.ctfassets.net", "assets.ctfassets.net"],
+  },
+};
+
+export default nextConfig;
