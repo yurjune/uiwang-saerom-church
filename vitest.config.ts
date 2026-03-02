8d41364196fc6baf1546f737b5cac04fdbb578d5
yurjune <yurjune@naver.com>
2026-03-03 04:15:15 +0900

    chore: testing library config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    environment: "jsdom",
     include: ["**/*.test.ts", "**/*.test.tsx"],
+    setupFiles: ["./vitest.setup.ts"],
   },
   resolve: {
     alias: {
