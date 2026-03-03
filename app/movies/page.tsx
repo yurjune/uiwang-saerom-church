7e93f815a748c7e47ef0779978ae008df4214262
yurjune <yurjune@naver.com>
2026-03-04 04:57:54 +0900

    feat: prevent bible filtered movies page indexing

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -8,9 +8,14 @@ import { ProjectMenu } from "@/constants/menu";
 import { Metadata } from "next/types";
 
 export const revalidate = 86400; // 1 day
-export const dynamic = "force-static";
 
-export const metadata: Metadata = {
+type PageProps = {
+  searchParams: Promise<{
+    [key: string]: string | string[] | undefined;
+  }>;
+};
+
+const baseMetadata: Metadata = {
   alternates: {
     canonical: ProjectUrl.movies.toString(),
   },
@@ -18,6 +23,25 @@ export const metadata: Metadata = {
   description: "설교영상과 말씀 콘텐츠를 확인할 수 있습니다.",
   keywords: [CHURCH_INFO.name, "설교영상", "주일예배"],
 };
+
+export async function generateMetadata({
+  searchParams: _searchParams,
+}: PageProps): Promise<Metadata> {
+  const searchParams = await _searchParams;
+  const hasQueryParams = Object.keys(searchParams).length > 0;
+  if (!hasQueryParams) {
+    return baseMetadata;
+  }
+
+  // 특정 성경 필터가 걸린 페이지의 색인 생성 방지
+  return {
+    ...baseMetadata,
+    robots: {
+      index: false,
+      follow: true,
+    },
+  };
+}
 
 export default async function Movies() {
   const articles = await getArticles({ category: CONTENTFUL_CATEGORY.movies });
