62cf9bcfa77d6405e2d4453706134be8b5b7a387
yurjune <yurjune@naver.com>
2026-03-04 00:40:26 +0900

    refactor: remove use media query

--- components/ContentsTable/ContentsTable.tsx
+++ components/ContentsTable/ContentsTable.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { useMediaQuery, useTheme } from "@chakra-ui/react";
 import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 import { categoryToContentUrl } from "@/utils/category";
 import { getLimitedArticles } from "@/utils/articles";
@@ -25,21 +24,21 @@ const ContentsTable = ({ articles: _articles, currentPage = 1 }: Props) => {
     currentPage,
     postNumberPerOnePage,
   );
-  const theme = useTheme();
-  const [isDesktop] = useMediaQuery(`(min-width: ${theme.breakpoints.md})`);
 
   return (
     <Table colorScheme="blackAlpha" variant="striped">
       <Thead>
         <Tr>
           <Th {...noWrap}>카테고리</Th>
-          {isDesktop && <Th>작성자</Th>}
+          <Th display={{ base: "none", md: "table-cell" }}>작성자</Th>
           <Th>제목</Th>
-          {isDesktop && (
-            <Th {...noWrap} isNumeric>
-              일시
-            </Th>
-          )}
+          <Th
+            {...noWrap}
+            isNumeric
+            display={{ base: "none", md: "table-cell" }}
+          >
+            일시
+          </Th>
         </Tr>
       </Thead>
 
@@ -49,7 +48,9 @@ const ContentsTable = ({ articles: _articles, currentPage = 1 }: Props) => {
             return (
               <Tr key={article.sys.id}>
                 <Td {...noWrap}>{article.fields.category}</Td>
-                {isDesktop && <Td {...noWrap}>관리자</Td>}
+                <Td {...noWrap} display={{ base: "none", md: "table-cell" }}>
+                  관리자
+                </Td>
                 <Td maxWidth={0} w="60%">
                   <Link
                     href={`${categoryToContentUrl(article.fields.category)}/${article.sys.id}`}
@@ -57,11 +58,13 @@ const ContentsTable = ({ articles: _articles, currentPage = 1 }: Props) => {
                     {article.fields.title}
                   </Link>
                 </Td>
-                {isDesktop && (
-                  <Td {...noWrap} isNumeric>
-                    {article.sys.createdAt.slice(0, 10)}
-                  </Td>
-                )}
+                <Td
+                  {...noWrap}
+                  isNumeric
+                  display={{ base: "none", md: "table-cell" }}
+                >
+                  {article.sys.createdAt.slice(0, 10)}
+                </Td>
               </Tr>
             );
           })}
