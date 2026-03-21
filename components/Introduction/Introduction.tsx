c082c7db3ab9fefaec17a5fd6be58ea8f30be084
yurjune <yurjune@naver.com>
2026-03-21 14:42:00 +0900

    feat: update introduction section design

--- components/Introduction/Introduction.tsx
+++ components/Introduction/Introduction.tsx
@@ -17,45 +17,39 @@ function Introduction() {
       <Box mx="auto" textAlign="center" position="relative" zIndex={1}>
         <Box
           as="h1"
-          mb={{ base: "40px", md: "60px" }}
-          fontSize={{ base: "26px", md: "3xl" }}
+          mb="40px"
+          fontSize={{ base: "20px", md: "3xl" }}
           fontWeight="bold"
           color="gray.900"
           textAlign="center"
           lineHeight="150%"
+          wordBreak="keep-all"
         >
           "너희가 서로 사랑하면 이로써 모든 사람이 너희가 내 제자인 줄 알리라"
-          <Text fontSize={{ base: "xl", md: "2xl" }}>(요 13:15)</Text>
+          <Text fontSize={{ base: "18px", md: "2xl" }}>(요 13:15)</Text>
         </Box>
 
         <Stack
-          spacing={{ base: 6, md: 8 }}
-          fontSize={{ md: "lg" }}
+          spacing={{ base: 2, md: 4 }}
+          fontSize={{ base: "sm", md: "lg" }}
           lineHeight="160%"
         >
           <Box>
-            <Text as="p" fontWeight="bold">
+            <Text as="p" fontWeight="semibold">
               하나님 말씀이 가치관과 삶의 중심이 되는 교회
             </Text>
-            <Text as="p">
-              Church where God&apos;s Word becomes the center of values and life
-            </Text>
           </Box>
 
           <Box>
-            <Text as="p" fontWeight="bold">
+            <Text as="p" fontWeight="semibold">
               나의 이웃과 성도 간에 사랑이 넘치는 교회
             </Text>
-            <Text as="p">
-              Church full of love between my neighbor and the saints
-            </Text>
           </Box>
 
           <Box>
-            <Text as="p" fontWeight="bold">
+            <Text as="p" fontWeight="semibold">
               모든 성도가 하나님을 섬기고 경배드리는 교회
             </Text>
-            <Text as="p">Church where all saints serve and worship God</Text>
           </Box>
         </Stack>
       </Box>
